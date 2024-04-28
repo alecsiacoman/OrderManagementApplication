9ce5162eab0f83e6e1d3f9ee98e1631c62c06eef
Alecsia <comanalecsiaa@gmail.com>
2024-04-28 16:31:45 +0300

    implement the ConnectionFactory class

--- src/main/java/com/example/pt2024_30423_coman_alecsia_assignment_3/Model/Client.java
+++ src/main/java/com/example/pt2024_30423_coman_alecsia_assignment_3/Model/Client.java
@@ -1,4 +1,55 @@
 package com.example.pt2024_30423_coman_alecsia_assignment_3.Model;
 
 public class Client {
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+    private int age;
+
+
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.pt2024_30423_coman_alecsia_assignment_3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.pt2024_30423_coman_alecsia_assignment_3 to javafx.fxml;
